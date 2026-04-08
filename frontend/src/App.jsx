6e4f9ea6804bb17459c21799169d8fc163c838dc
Tanuj Narnam <tanujnarnam@tamu.edu>
2026-04-08 08:36:26 -0500

    Added Oauth Success page to router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -46,7 +46,7 @@ function App() {
               <Route path="/login" element={<Login />} />
               <Route path="/register" element={<Register />} />
               <Route path="/about" element={<About />} />
-
+              <Route path="/oauth-success" element={<OAuthSuccess />} />
 
               {/* Protected routes */}
               <Route
